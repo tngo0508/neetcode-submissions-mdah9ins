37b1784148d07d345bdf82c9d951facb7226521e
Thomas Ngo <tngo0508@gmail.com>
2026-04-17 15:10:25 -0700

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int Trap(int[] height) {
+        int n = height.Length;
+        int res = 0;
+        int[] leftArr = new int[n];
+        int currHeight = 0;
+        for (int i = 0; i < n; i++) {
+            currHeight = Math.Max(currHeight, height[i]);
+            leftArr[i] = currHeight;
+        }
+        currHeight = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            currHeight = Math.Max(currHeight, height[i]);
+            res += (Math.Min(leftArr[i], currHeight) - height[i]);
+        }
+        return res;
+    }
+}

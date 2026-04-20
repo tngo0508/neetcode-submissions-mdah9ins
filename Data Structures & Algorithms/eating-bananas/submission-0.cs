43905aedaa26553e1809a90a9c74180a5924cfae
Thomas Ngo <tngo0508@gmail.com>
2026-04-19 20:20:58 -0700

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public bool IsValid(int amount, int[] piles, int target)
+{
+    int totalTime = 0;
+    foreach (int pile in piles)
+    {
+        int hour = (int)Math.Ceiling((double)pile / amount);
+        totalTime += hour;
+    }
+
+    return totalTime <= target;
+}
+
+public int MinEatingSpeed(int[] piles, int h)
+{
+    int left = 1, right = piles.Max();
+    int ans = 1;
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+        Console.WriteLine($"left: {left}, right: {right}, mid: {mid}");
+        if (IsValid(mid, piles, h))
+        {
+            ans = mid;
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+
+    return ans;
+}
+}
